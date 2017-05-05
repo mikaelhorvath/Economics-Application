82f42c9d92fe15e1435eface758dd3c7db339816
mikaelhorvath <hejhopp_miki@hotmail.com>
2017-05-05 14:18:36 +0200

    DB Method added

--- src/controller/AWSController.java
+++ src/controller/AWSController.java
@@ -2,6 +2,11 @@ package controller;
 
 import objects.DBObj;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * This class handles database events from Amazon Web Services
  * Created by mikaelhorvath on 2017-05-04.
@@ -12,6 +17,7 @@ public class AWSController {
 
     public AWSController(DBObj d){
         this.dbO = d;
+        uploadToAWS();
     }
     /**
      * Sets Controller class
@@ -36,7 +42,25 @@ public class AWSController {
      * Method uses SQL logic to upload information to AWS RDS Database
      */
     private void uploadToAWS(){
+        String url = "jdbc:mysql://iotapdb.cr5wcbntqpwo.eu-central-1.rds.amazonaws.com:3306/iotapdb";
+        String username = "mikaelhorvath";
+        String password = "angelica";
+        Connection connection = null;
 
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+            Statement stmt = connection.createStatement();
+            String namez = dbO.getName();
+            String descr = dbO.getDesc();
+            String emailz = dbO.getEmail();
+            int sumz = dbO.getSum();
+            // Not working just yet!
+            stmt.executeUpdate("INSERT INTO `Expenses`(Namn,Description,Mejl,Summa) VALUE ('"+namez+"','"+descr+"','"+emailz+"',"+sumz+")");
+            stmt.close();
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException("Cannot connect the database!", e);
+        }
     }
 
     /**
